546854b40d7dbd153b626317362c63b97029530a
mleuchtenmueller <LeuchtenmuellerM1981@sus.htl-grieskirchen.at>
2021-11-01 15:31:14 +0100

    Mostly finished. 1.3 and 2.6 not functioning correct

--- Hue3/src/hue3/Beispiel4.java
+++ Hue3/src/hue3/Beispiel4.java
@@ -1,10 +1,10 @@
 package hue3;
 
-import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
 public class Beispiel4 {
     public int squareOddNumbersAndCalcSum(){
-        final int result = DoubleStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).filter(x -> x%2 != 0).map(x -> Math.pow(x, 2)).reduce(0, Integer::sum);
+        final int result = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).filter(x -> x%2 != 0).map(x -> (int)x*x).reduce(0, Integer::sum);
         return result;
     }
 }
